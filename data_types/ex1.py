bfc8f3d222e06da269752ca5599adfad12172a72
Aaron Hoyos <127354521+chochimus@users.noreply.github.com>
2025-06-16 19:16:54 -0500

    Completed ex1

---
+++ data_types/ex1.py
@@ -0,0 +1,10 @@
+# Value    ``````Type
+# 'True'         string
+# False          boolean
+# (1,2,3)        tuple
+# 1.5            float
+# [1,2,3]        list
+# 2              integer
+# range(5)       range
+# None           NoneType
+# {'foo': 'bar'} dictionary
