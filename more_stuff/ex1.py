922d344bf7ed5c45f8ba2386c4d942160435f84f
Aaron Hoyos <127354521+chochimus@users.noreply.github.com>
2025-06-21 02:45:11 -0500

    Completed exercises for more_stuff

---
+++ more_stuff/ex1.py
@@ -0,0 +1,2 @@
+# sorts the keys of a dictionary, returns the key at index 1
+# in uppercase
---
+++ more_stuff/ex2.py
@@ -0,0 +1,6 @@
+import math
+
+print(math.sqrt(37))
+
+# or from math import sqrt
+# or import math as m
